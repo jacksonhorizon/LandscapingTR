a4a0fdcb08f6a0d55719e2610b45ee2c15ddf051
jacksonhorizon <98056535+jacksonhorizon@users.noreply.github.com>
2023-09-26 16:37:11 -0700

    Add TransactionScope Factory

---
+++ LandScapingTRCore/Factories/TransactionScopeFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Transactions;
+
+namespace LandscapingTR.Core.Factories
+{
+    public static class TransactionScopeFactory
+    {
+        public static TransactionScope createReadUncommitted()
+        {
+            return createReadUncommitted(TransactionScopeOption.Required);
+        }
+
+        public static TransactionScope createReadUncommitted(TransactionScopeOption scopeOption)
+        {
+            var options = new TransactionOptions()
+            {
+                IsolationLevel = IsolationLevel.ReadUncommitted
+            };
+
+            return new TransactionScope(scopeOption, options, TransactionScopeAsyncFlowOption.Enabled);
+        }
+    }
+}
