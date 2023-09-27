72792c6a7c9f633eb39c260b44e74ef2280af2e2
jacksonhorizon <98056535+jacksonhorizon@users.noreply.github.com>
2023-09-27 10:38:56 -0700

    Create methods in lookup repositories to retrieve lookups from actual production database (Sus)

--- LandScapingTRCore/Interfaces/ILookupRepository.cs
+++ LandScapingTRCore/Interfaces/ILookupRepository.cs
@@ -4,5 +4,31 @@ namespace LandscapingTR.Core.Interfaces
 {
     public interface ILookupRepository : IRepository<BaseLookupEntity, int?>
     {
+        /// <summary>
+        /// Gets the job types.
+        /// </summary>
+        /// <returns>The job types.</returns>
+        Task<List<JobType>> GetJobTypesAsync();
+
+        /// <summary>
+        /// Gets the employee types.
+        /// </summary>
+        /// <returns>The employee types.</returns>
+        Task<List<EmployeeType>> GetEmployeeTypesAsync();
+
+
+        /// <summary>
+        /// Gets the location types.
+        /// </summary>
+        /// <returns>The location types.</returns>
+        Task<List<LocationType>> GetLocationTypesAsync();
+
+
+        /// <summary>
+        /// Gets the customer types.
+        /// </summary>
+        /// <returns>The customer types.</returns>
+        Task<List<CustomerType>> GetCustomerTypesAsync();
+
     }
 }
---
+++ LandscapingTRTest/LookupsUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandscapingTR.Core.Interfaces;
+using LandscapingTR.Infrastructure;
+using LandscapingTR.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace LandscapingTR.Test
+{
+    [TestClass]
+    public class LookupsRepositoryUnitTest
+    {
+
+        private static ILookupRepository lookupRepository;
+
+        private static LandscapingTRDbContext context;
+     
+
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            DbContextOptions<LandscapingTRDbContext> options;
+            var builder = new DbContextOptionsBuilder<LandscapingTRDbContext>();
+            builder.UseSqlServer("Server=.\\SQLEXPRESS;Database=LandscapingTRDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            options = builder.Options;
+            context = new LandscapingTRDbContext(options);
+
+            lookupRepository = new LookupRepository(context);
+        }
+
+        [ClassCleanup]
+        public static void TestCleanup()
+        {
+            // Class Cleanup
+        }
+
+        [TestMethod]
+        public async Task LookupRepository_GetJobTypes_Succeeds()
+        {
+            var lookupEntities = await lookupRepository.GetJobTypesAsync();
+            Assert.AreEqual(11, lookupEntities.Count);
+        }
+
+        [TestMethod]
+        public async Task LookupRepository_GetLocationTypes_Succeeds()
+        {
+            var lookupEntities = await lookupRepository.GetLocationTypesAsync();
+            Assert.AreEqual(5, lookupEntities.Count);
+        }
+
+        [TestMethod]
+        public async Task LookupRepository_GetEmployeeTypes_Succeeds()
+        {
+            var lookupEntities = await lookupRepository.GetEmployeeTypesAsync();
+            Assert.AreEqual(5, lookupEntities.Count);
+        }
+
+        [TestMethod]
+        public async Task LookupRepository_GetCustomerTypes_Succeeds()
+        {
+            var lookupEntities = await lookupRepository.GetCustomerTypesAsync();
+            Assert.AreEqual(3, lookupEntities.Count);
+        }
+    }
+}
