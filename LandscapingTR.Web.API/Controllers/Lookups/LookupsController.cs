797ff6e7baa9659d7adcd52f30d54c18b8feaccf
jacksonhorizon <98056535+jacksonhorizon@users.noreply.github.com>
2023-09-27 16:20:45 -0700

    Add WebApi Project

---
+++ LandscapingTR.Web.API/Controllers/Lookups/LookupsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LandscapingTR.Core.Models.CompanyResources;
+using LandscapingTR.Core.Interfaces;
+
+namespace LandscapingTR.Web.API.Controllers.Lookups
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookupsController : ControllerBase
+    {
+        private readonly ILookupService lookupService;
+
+        public LookupsController(ILookupService lookupService)
+        {
+            this.lookupService = lookupService;
+        }
+
+        [HttpGet]
+        [Route("JobTypes")]
+        public async Task<IActionResult> GetJobTypes()
+        {
+            var lookupModels = await this.lookupService.GetJobTypesAsync();
+
+            if (lookupModels.Count == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(lookupModels);
+        }
+
+        [HttpGet]
+        [Route("CustomerTypes")]
+        public async Task<IActionResult> GetCustomerTypes()
+        {
+            var lookupModels = await this.lookupService.GetCustomerTypesAsync();
+
+            if (lookupModels.Count == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(lookupModels);
+        }
+
+        [HttpGet]
+        [Route("EmployeeTypes")]
+        public async Task<IActionResult> GetEmployeeTypes()
+        {
+            var lookupModels = await this.lookupService.GetEmployeeTypesAsync();
+
+            if (lookupModels.Count == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(lookupModels);
+        }
+
+        [HttpGet]
+        [Route("LocationTypes")]
+        public async Task<IActionResult> GetLocationTypes()
+        {
+            var lookupModels = await this.lookupService.GetLocationTypesAsync();
+
+            if (lookupModels.Count == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(lookupModels);
+        }
+    }
+}
---
+++ LandscapingTR.Web.API/Program.cs
@@ -0,0 +1,69 @@
+using System.Reflection;
+using AutoMapper;
+using LandscapingTR.Core;
+using LandscapingTR.Core.Interfaces;
+using LandscapingTR.Core.Services;
+using LandscapingTR.Infrastructure;
+using LandscapingTR.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Automapper config
+var mapperConfig = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new AutoMapperProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+// Add services to the container.
+builder.Services.AddDbContext<LandscapingTRDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LandscapingTRConnectionString")));
+builder.Services.AddTransient<IMapper>(_ => mapper);
+builder.Services.AddTransient<ILookupRepository, LookupRepository>();
+builder.Services.AddTransient<ILookupService, LookupService>();
+
+
+
+
+
+
+// Add other Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "LandScapingTR.Web.API",
+            Description = "The Web API for LandscapingTR",
+            Version = "v1"
+        });
+
+    var filename = Assembly.GetExecutingAssembly().GetName().Name + ".xml";
+    var filepath = Path.Combine(AppContext.BaseDirectory, filename);
+});
+
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+        options.DocumentTitle = "My Swagger";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
